dd798e981fca7c0e3292bfbab8aff94b96b97df6
Andres Liu <andres.liu22@gmail.com>
2021-10-21 14:40:25 -0700

    Delete blankspaces

--- server.js
+++ server.js
@@ -15,8 +15,6 @@ app.use('/api', api);
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public/notes.html')));
 
-
-
 app.listen(PORT, () => {
     console.log(`Listening from port ${PORT}!`);
 })
