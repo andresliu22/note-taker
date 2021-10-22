8158b76b6f61600d3b7308a4cdbd2813aba3f2f9
Andres Liu <andres.liu22@gmail.com>
2021-10-22 10:36:51 -0700

    Add Wildcard route

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ app.use('/api', api);
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public/notes.html')));
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
 
 app.listen(PORT, () => {
     console.log(`Listening from port ${PORT}!`);
